06370c759e2a4cddd419001ff268f517b477a9fd
Phil Marsden <phil.marsden@softwire.com>
2018-01-15 15:40:53 +0000

    Scroll up

--- floor/processor/message.py
+++ floor/processor/message.py
@@ -1,8 +1,10 @@
+from utils import clocked
 from base import Base
 import os.path
 import colorsys
 import importlib
-
+import logging
+logger = logging.getLogger('message')
 
 class Message(Base):
 
@@ -11,7 +13,7 @@ class Message(Base):
 	# How far apart should the letters be
 	KERNING = 1
 	# File with messages to display
-	MESSAGE_FILE = "/tmp/messages.txt"
+	MESSAGE_FILE = "floor\config\messages.txt"
 	# Default message to show if messages file is empty or missing
 	DEFAULT_MESSAGE = "Burn baby burn, Disco Inferno"
 
@@ -37,9 +39,7 @@ class Message(Base):
 		# The message converted to the font in an array
 		self.wall = []
 		# The current window position on the wall
-		self.window = 0.0
-		# How fast to scroll in pixels per frame
-		self.speed = 0.33
+		self.window = 0
 
 		if "text" in kwargs:
 			# Load a single message from the args
@@ -48,7 +48,11 @@ class Message(Base):
 			# Load a list of messages from the messages file
 			self.load_messages()
 
-		self.load_next_wall()
+	def is_clocked(self):
+		return True
+
+	def initialise_processor(self):
+		self.load_wall()
 
 	def load_messages(self):
 		if os.path.isfile(self.MESSAGE_FILE):
@@ -68,36 +72,45 @@ class Message(Base):
 	def load_default_message(self):
 		self.messages = [self.DEFAULT_MESSAGE]
 
-	def load_next_wall(self):
+	def load_wall(self):
 		self.wall = []
-		self.window = 0.0
-		mesg = self.messages[self.message_num]
+		self.window = 0.0	
+		wall_index = 0
+		logger.debug("Adding {} blank lines to wall".format(self.FLOOR_HEIGHT))
+		for row in range(self.FLOOR_HEIGHT):
+				logger.debug("Adding blank to wall_index {}".format(wall_index))
+				self.wall.append([])
+				self.wall[wall_index].extend([0] * self.FLOOR_WIDTH)
+				wall_index = wall_index + 1
+		for line in range(len(self.messages)):
+			mesg = self.messages[line]
+			logger.debug("Line {} : {}".format(line,mesg))
+			for row in range(0, 8):
+				logger.debug("Processing wall_index {}".format(wall_index))
+				self.wall.append([])
+				# Add the characters for this row
+				self.load_wall_row(mesg, row, wall_index)
+				self.wall[row].extend([0] * self.FLOOR_WIDTH)
 
-		for row in range(0, 8):
-			self.wall.append([])
+				# Add blank space at the end so the text can scroll off the side, minus the
+				# kerning from the final letter
+				#self.wall[row].extend([0] * (self.FLOOR_WIDTH - self.KERNING))
+				wall_index = wall_index + 1
+		for row in range(self.FLOOR_HEIGHT):
+				self.wall.append([])
+				self.wall[wall_index].extend([0] * self.FLOOR_WIDTH)
+				wall_index = wall_index + 1
 
-			# Add blank space at the beginning so the text can scroll in from the side
-			self.wall[row].extend([0] * self.FLOOR_WIDTH)
-
-			# Add the characters for this row
-			self.load_wall_row(mesg, row)
-
-			# Add blank space at the end so the text can scroll off the side, minus the
-			# kerning from the final letter
-			self.wall[row].extend([0] * (self.FLOOR_WIDTH - self.KERNING))
-
-		# Ready us for the next load
-		self.message_num = (self.message_num + 1) % len(self.messages)
-
-	def load_wall_row(self, mesg, row):
+	def load_wall_row(self, mesg, row, wall_index):
 		for char in list(mesg):
 			char_data = self.get_font_char(char)
 			row_data = char_data[row]
 
 			# Add this strip of the character to the wall
-			self.wall[row].extend(row_data)
+			self.wall[wall_index].extend(row_data)
 			# Add kerning
-			self.wall[row].extend([0] * self.KERNING)
+			self.wall[wall_index].extend([0] * self.KERNING)
+		self.wall[wall_index].extend([0] * self.FLOOR_WIDTH)
 
 	def get_font_char(self, char):
 		if char in self.font:
@@ -120,25 +133,22 @@ class Message(Base):
 
 		return rgb
 
+	@clocked(frames_per_beat=4)
 	def get_next_frame(self, weights):
 		# Ignore weights
-
 		pixels = []
-		for row in range(0, 8):
-			for col in range(int(self.window), int(self.window)+self.FLOOR_WIDTH):
-				if self.wall[row][col]:
+		for row in range(self.FLOOR_HEIGHT):
+			for col in range(self.FLOOR_WIDTH):
+				#logger.debug("row:{}, x:{}".format(row+self.window,col))
+				if self.wall[int(row+self.window)][col]:
 					pixels.append(self.current_rgb_tuple())
 				else:
 					pixels.append((0, 0, 0))
 
-		for row in range(0, self.FLOOR_HEIGHT -8):
-			for col in range(int(self.window), int(self.window)+self.FLOOR_WIDTH):
-				pixels.append((0, 0, 0))
 
 		self.shift_color()
-
-		self.window += self.speed
-		if self.window >= len(self.wall[0]) - self.FLOOR_WIDTH:
-			self.load_next_wall()
+		self.window += 1
+		if (self.window + self.FLOOR_HEIGHT) >= len(self.wall):
+			self.window = 0
 
 		return pixels
