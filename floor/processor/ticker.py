c55e471c48f2d77827e8c6deeb30f6fea0b73ebd
Harry Cummings <harry.cummings@softwire.com>
2018-08-30 16:21:13 +0100

    Update ticker to show message types and multiple messages

--- floor/processor/ticker.py
+++ floor/processor/ticker.py
@@ -4,9 +4,44 @@ import logging
 import importlib
 logger = logging.getLogger('ticker')
 
-RED = (0xff, 0x00, 0x00)
-GREEN = (0x00, 0xff, 0x00)
-BLUE = (0, 0, 0xff)
+ICONS = {
+	'announcement': (
+		((0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (192,192,192)),
+		((0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (255,255,255), (192,192,192)),
+		((0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (255,255,255), (255,255,255), (255,255,255), (192,192,192)),
+		((255, 0, 0), (255, 0, 0), (255,255,255), (255,255,255), (255,255,255), (255,255,255), (255,255,255), (192,192,192)),
+		((255, 0, 0), (255, 0, 0), (255,255,255), (255,255,255), (255,255,255), (255,255,255), (255,255,255), (192,192,192)),
+		((0, 0, 0), (128,128,128), (0, 0, 0), (0, 0, 0), (255,255,255), (255,255,255), (255,255,255), (192,192,192)),
+		((0, 0, 0), (128,128,128), (128,128,128), (0, 0, 0), (0, 0, 0), (0, 0, 0), (255,255,255), (192,192,192)),
+		((0, 0, 0), (128,128,128), (128,128,128), (0, 0, 0), (0, 0, 0), (0, 0, 0), (0, 0, 0), (192,192,192))
+	),
+	'praise': (
+		((0, 0, 0), (0, 0, 0), (255,216,0), (255,216,0), (255,216,0), (255,216,0), (0, 0, 0), (0, 0, 0)),
+		((255,216,0), (255,216,0), (255,216,0), (255,216,0), (255,216,0), (255,216,0), (255,216,0), (255,216,0)),
+		((255,216, 0), (0, 0, 0), (255,216,0), (255,216,0), (255,216,0), (255,216,0), (0, 0, 0), (255,216, 0)),
+		((0, 0, 0), (255,216,0), (255,216,0), (255,216,0), (255,216,0), (255,216,0), (255,216,0), (0, 0, 0)),
+		((0, 0, 0), (0, 0, 0), (255,216,0), (255,216,0), (255,216,0), (255,216,0), (0, 0, 0), (0, 0, 0)),
+		((0, 0, 0), (0, 0, 0), (0, 0, 0), (255,216,0), (255,216,0), (0, 0, 0), (0, 0, 0), (0, 0, 0)),
+		((0, 0, 0), (0, 0, 0), (0, 0, 0), (255,216,0), (255,216,0), (0, 0, 0), (0, 0, 0), (0, 0, 0)),
+		((0, 0, 0), (255,216,0), (255,216,0), (255,216,0), (255,216,0), (255,216,0), (255,216,0), (0, 0, 0))
+	),
+	'sale': (
+		((0, 0, 0), (0, 0, 0), (0, 0, 0), (255,216,0), (255,216,0), (0, 0, 0), (0, 0, 0), (0, 0, 0)),
+		((0, 0, 0), (0, 0, 0), (255,216,0), (255,216,0), (255,216,0), (255,216,0), (0, 0, 0), (0, 0, 0)),
+		((0, 0, 0), (255,216,0), (255,216,0), (255,216,0), (255,216,0), (255,216,0), (255,216,0), (0, 0, 0)),
+		((0, 0, 0), (255,216,0), (255,216,0), (255,216,0), (255,216,0), (255,216,0), (255,216,0), (0, 0, 0)),
+		((0, 0, 0), (255,216,0), (255,216,0), (255,216,0), (255,216,0), (255,216,0), (255,216,0), (0, 0, 0)),
+		((255,216,0), (255,216,0), (255,216,0), (255,216,0), (255,216,0), (255,216,0), (255,216,0), (255,216,0)),
+		((255,216,0), (127,106,0), (127,106,0), (127,106,0), (127,106,0), (127,106,0), (127,106,0), (255,216,0)),
+		((0, 0, 0), (0, 0, 0), (0, 0, 0), (255,216,0), (255,216,0), (0, 0, 0), (0, 0, 0), (0, 0, 0))
+	)
+}
+
+MESSAGES = [
+	{ 'type': 'announcement', 'text': 'Watch this space' },
+	{ 'type': 'praise', 'text': 'Thanks to Phil for creating the dancefloor' },
+	{ 'type': 'sale', 'text': 'Congratulations to the sales team for closing another deal' }
+]
 
 class Ticker(Base):
 	DEFAULT_FONT = "synchronizer"
@@ -16,7 +51,7 @@ class Ticker(Base):
 		logger.debug('__init__')
 		# Set up any instance variables
 		self.brightness = 255
-		self.current_message = "Watch this space..."
+		self.message_index = 0
 		self.font = importlib.import_module("processor.fonts.{}".format(self.DEFAULT_FONT))
 	
 	# Optional initialiser that is called once when the class is first created.  The following base variables are available in this function (but not in the __init__ constructor)
@@ -25,7 +60,7 @@ class Ticker(Base):
 	# If you want to pre-calculate frames then you can do so in here and store the output of self.get_raw_pixel_data()
 	def initialise_processor(self):
 		logger.debug('initialise_processor')
-		self.render_message()
+		self.next_message()
 		
 
 # get_next_frame must either
@@ -39,18 +74,23 @@ class Ticker(Base):
 # - Return an array of (R,G,B) objects of length self.FLOOR_HEIGHT * self.FLOOR_WIDTH
 # - Return None and have called self.set_raw_pixel_data() using data obtained in the initialise_processor function
 # - Return None and have called self.set_pixel (x,y,color) for all the pixels that have changed
-	@clocked(frames_per_beat=6)
 	def get_next_frame(self, weights):
 		pixels = []
 
 		pixels.extend([(0, 0, 0) for x in range(0, self.FLOOR_WIDTH)])
 		
 		for row_index in range(0, self.font.height()):
-			row_pixels = self.current_text[row_index]
-			for column_index in range(0, self.FLOOR_WIDTH):
+			icon_pixels = self.current_icon[row_index]
+			pixels.append((0,0,0))
+			pixels.extend(icon_pixels)
+			pixels.append((0,0,0))
+			margin = 1 + len(icon_pixels) + 1
+
+			text_pixels = self.current_text[row_index]
+			for column_index in range(0, self.FLOOR_WIDTH - margin):
 				pixel_index = column_index + self.current_offset
 				pixel = (0, 0, 0)
-				if (pixel_index < len(row_pixels) and row_pixels[pixel_index]):
+				if (pixel_index < len(text_pixels) and text_pixels[pixel_index]):
 					pixel = (255, 255, 255)
 				pixels.append(pixel)
 		
@@ -59,15 +99,24 @@ class Ticker(Base):
 
 		self.current_offset += 1
 
+		if (self.current_offset > len(self.current_text[0])):
+			self.next_message()
+
 		return pixels
 
-	def render_message(self):
+	def next_message(self):
+		self.message_index = (self.message_index + 1) % len(MESSAGES)
+		self.render_message(MESSAGES[self.message_index])
+
+	def render_message(self, message):
 		self.current_text = []
 		self.current_offset = 0
 
+		self.current_icon = ICONS[message['type']]
+
 		for row_index in range(0, self.font.height()):
 			self.current_text.append([])
-			for char in list(self.current_message):
+			for char in list(message['text']):
 				char_pixels = self.get_font_char(char)
 				self.current_text[row_index].append(0)
 				self.current_text[row_index].extend(char_pixels[row_index])
@@ -77,3 +126,5 @@ class Ticker(Base):
 			return self.font.alpha()[char]
 		else:
 			return self.font.alpha()[" "]
+
+
