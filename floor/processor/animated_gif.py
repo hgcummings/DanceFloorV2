a6874c6d892f6ff599536703364f8d5e54f7542b
Harry Cummings <harry.cummings@softwire.com>
2018-12-09 20:21:16 +0000

    Add caching to animated gif processor

--- floor/processor/animated_gif.py
+++ floor/processor/animated_gif.py
@@ -9,6 +9,7 @@ import urllib
 import sys
 import logging
 logger = logging.getLogger('animatedgif')
+CACHE = {}
 
 class AnimatedGIF(Base):
 	""" Displays a single animated gif, respecting frame duration and loop metadata
@@ -28,25 +29,39 @@ class AnimatedGIF(Base):
 		logger.info("File : {}".format(kwargs["file"]))
 
 	def initialise_processor(self):
-		logger.info("======= Processing File {} ".format(self.file))
-		im_in = Image.open(self.file)
-	
-		if ((im_in.size[0] != self.FLOOR_WIDTH) or (im_in.size[1] != self.FLOOR_HEIGHT)):
-			raise Exception("Image must be {}x{}".format(self.FLOOR_WIDTH, self.FLOOR_HEIGHT))
+		global CACHE
+		if self.file in CACHE:
+			logger.info("======= Using cached version of file {}".format(self.file))
+			cached = CACHE[self.file]
+			self.loop = cached['loop']
+			self.frame_data = cached['frame_data']
+			self.frame_durations = cached['frame_durations']
 		else:
-			im_in.seek(0)
-			self.loop = 'loop' in im_in.info
-			try:
-				while True:
-					frame = im_in.convert('RGBA')
-					index = im_in.tell()
-					logger.info("Processing {} frame from file : {} {}x{}".format(index,self.file,frame.size[0],frame.size[1]))
-					self.show_image(frame)
-					self.frame_data.append(self.get_raw_pixel_data())
-					self.frame_durations.append(im_in.info['duration'])
-					im_in.seek(index + 1)
-			except:
-				pass
+			logger.info("======= Processing File {} ".format(self.file))
+			im_in = Image.open(self.file)
+		
+			if ((im_in.size[0] != self.FLOOR_WIDTH) or (im_in.size[1] != self.FLOOR_HEIGHT)):
+				raise Exception("Image must be {}x{}".format(self.FLOOR_WIDTH, self.FLOOR_HEIGHT))
+			else:
+				im_in.seek(0)
+				self.loop = 'loop' in im_in.info
+				try:
+					while True:
+						frame = im_in.convert('RGBA')
+						index = im_in.tell()
+						logger.info("Processing {} frame from file : {} {}x{}".format(index,self.file,frame.size[0],frame.size[1]))
+						self.show_image(frame)
+						self.frame_data.append(self.get_raw_pixel_data())
+						self.frame_durations.append(im_in.info['duration'])
+						im_in.seek(index + 1)
+				except:
+					pass
+
+			CACHE[self.file] = {
+				'loop': self.loop,
+				'frame_data': self.frame_data,
+				'frame_durations': self.frame_durations
+			}
 
 	def get_next_frame(self, weights):
 		current_millis = int(time.time() * 1000)
