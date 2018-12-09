8c64e647fbd6a3d0d4ae4379f0b6211a5a15d647
Harry Cummings <harry.cummings@softwire.com>
2018-12-09 16:07:52 +0000

    Tweak panto architect processor

--- floor/processor/panto_architect.py
+++ floor/processor/panto_architect.py
@@ -1,24 +1,18 @@
 from base import Base
 from utils import clocked
 import logging
+import math
 import random
+import time
 logger = logging.getLogger('pantoarchitect')
 
 class PantoArchitect(Base):
-	MINS = [
-		[8,16,32,32,32,16,8,0],
-		[16,32,64,64,64,64,16,0],
-		[32,64,64,64,64,64,32,0],
-		[16,64,64,64,64,64,16,0],
-		[8,16,32,32,32,16,8,0],
-		[0,0,0,0,0,0,0,0]
-	] 
-	MAXS = [
-		[32,64,128,128,128,64,32,0],
-		[64,128,255,255,255,128,64,0],
-		[64,255,255,255,255,255,64,0],
-		[64,128,255,255,255,128,64,0],
-		[32,64,128,128,128,64,32,0],
+	RELS = [
+		[2,4,6,6,6,4,2,0],
+		[4,7,8,8,8,7,4,0],
+		[5,8,8,8,8,8,5,0],
+		[4,7,8,8,8,7,4,0],
+		[2,4,6,6,6,4,2,0],
 		[0,0,0,0,0,0,0,0]
 	]
 
@@ -29,13 +23,15 @@ class PantoArchitect(Base):
 	def is_clocked(self):
 		return True
 		
-	@clocked(frames_per_beat=1)
 	def get_next_frame(self, weights):
+		millis = time.time()
+		base_brightness = ((math.cos((millis - math.floor(millis)) * math.pi)) + 3) * 8
 		frame = [None] * self.FLOOR_HEIGHT * self.FLOOR_WIDTH
 		for y in range(0, self.FLOOR_HEIGHT):
 			for x in range(0, self.FLOOR_WIDTH):
 				rel_x = x % 8 
-				rel_y = y % 6 
-				brightness = random.randint(self.MINS[rel_y][rel_x], self.MAXS[rel_y][rel_x])
+				rel_y = y % 6
+				rel = self.RELS[rel_y][rel_x]
+				brightness = min(255, base_brightness * rel + random.randint(0, rel))
 				frame[self.idx((x, y))] = (brightness, brightness, brightness)
 		return frame
