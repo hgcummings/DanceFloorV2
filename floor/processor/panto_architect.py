0e1563b2c05a32083f1c93037b5596a94f0511f3
Harry Cummings <harry.cummings@softwire.com>
2018-12-09 16:11:38 +0000

    Further tweaks to panto architect processor

--- floor/processor/panto_architect.py
+++ floor/processor/panto_architect.py
@@ -8,11 +8,11 @@ logger = logging.getLogger('pantoarchitect')
 
 class PantoArchitect(Base):
 	RELS = [
-		[2,4,6,6,6,4,2,0],
-		[4,7,8,8,8,7,4,0],
-		[5,8,8,8,8,8,5,0],
-		[4,7,8,8,8,7,4,0],
-		[2,4,6,6,6,4,2,0],
+		[1,2,4,4,4,2,1,0],
+		[2,4,8,8,8,4,2,0],
+		[4,6,8,8,8,6,4,0],
+		[2,4,8,8,8,4,2,0],
+		[1,2,4,4,4,2,1,0],
 		[0,0,0,0,0,0,0,0]
 	]
 
@@ -25,13 +25,13 @@ class PantoArchitect(Base):
 		
 	def get_next_frame(self, weights):
 		millis = time.time()
-		base_brightness = ((math.cos((millis - math.floor(millis)) * math.pi)) + 3) * 8
+		base_brightness = ((math.cos((millis - math.floor(millis)) * math.pi / 2)) + 3) * 8
 		frame = [None] * self.FLOOR_HEIGHT * self.FLOOR_WIDTH
 		for y in range(0, self.FLOOR_HEIGHT):
 			for x in range(0, self.FLOOR_WIDTH):
 				rel_x = x % 8 
 				rel_y = y % 6
 				rel = self.RELS[rel_y][rel_x]
-				brightness = min(255, base_brightness * rel + random.randint(0, rel))
+				brightness = min(255, base_brightness * rel + random.randint(0, rel) * 4)
 				frame[self.idx((x, y))] = (brightness, brightness, brightness)
 		return frame
